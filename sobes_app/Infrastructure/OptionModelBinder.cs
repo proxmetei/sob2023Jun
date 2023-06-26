30971f992b5dd79c99e81f1b481012b546956f67
akorchagin <aleksei.korchagin200@mail.ru>
2023-06-26 19:24:05 +0300

    Model Binding added

---
+++ sobes_app/Infrastructure/OptionModelBinder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sobes_app.Models;
+
+namespace sobes_app.Infrastructure
+{
+    public class OptionModelBinder : IModelBinder
+    {
+        public object BindModel
+          (
+            ControllerContext controllerContext,
+            ModelBindingContext bindingContext
+          )
+        {
+            var options = new Options();
+            List<string> opt = new List<string>(); 
+            foreach (var option in Enum.GetValues(typeof(sobes_app.Models.Options)))
+            {
+                if (GetValue(bindingContext, option.ToString()) != null)
+                    options = options | (Options)option;
+            }
+            return options;
+        }
+
+        private string GetValue(
+          ModelBindingContext bindingContext, string key)
+        {
+            var result = bindingContext.ValueProvider.GetValue(key);
+            return (result == null) ? null : result.AttemptedValue;
+        }
+    }
+}
